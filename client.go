186e261144ada4bc4cc202ca2ed05dad22a61aa8
Daniel Schmidt <bigjk55@gmail.com>
2016-12-06 17:53:47 +0100

    Working multicast receive and send.

--- client.go
+++ client.go
@@ -7,29 +7,42 @@ import (
 	"golang.org/x/net/ipv4"
 )
 
+var globalPort int
 var globalConn *ipv4.PacketConn
+var globalGroup net.IP
+var networkInterfaces []net.Interface
 
-func initServer() {
+func initServer(port int) {
+	globalPort = port
+	globalGroup = net.IPv4(224, 0, 0, 250)
+
 	is, _ := net.Interfaces()
-	group := net.IPv4(224, 0, 0, 250)
-	c, _ := net.ListenPacket("udp4", "0.0.0.0:1024")
+	networkInterfaces = is
+
+	c, _ := net.ListenPacket("udp4", "0.0.0.0:"+fmt.Sprint(globalPort))
 	globalConn = ipv4.NewPacketConn(c)
 
 	for i := 0; i < len(is); i++ {
-		globalConn.JoinGroup(&is[i], &net.UDPAddr{IP: group})
+		globalConn.JoinGroup(&is[i], &net.UDPAddr{IP: globalGroup})
 	}
-
-	globalConn.SetControlMessage(ipv4.FlagDst, true)
 
 	b := make([]byte, 1500)
 	for {
-		n, cm, src, _ := globalConn.ReadFrom(b)
-		if cm.Dst.IsMulticast() {
-			if cm.Dst.Equal(group) {
-				fmt.Println(n, src)
-			} else {
-				continue
-			}
+		n, _, src, _ := globalConn.ReadFrom(b)
+		fmt.Println(b, n, src)
+	}
+}
+
+func sendData(data []byte) error {
+	dst := &net.UDPAddr{IP: globalGroup, Port: globalPort}
+	for i := 0; i < len(networkInterfaces); i++ {
+		if err := globalConn.SetMulticastInterface(&networkInterfaces[i]); err != nil {
+			return err
+		}
+		globalConn.SetMulticastTTL(2)
+		if _, err := globalConn.WriteTo([]byte{1, 3, 3, 7}, nil, dst); err != nil {
+			return err
 		}
 	}
+	return nil
 }
