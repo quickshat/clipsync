4bbe5c2382297401510bc19971bb09a28986d031
Daniel Schmidt <bigjk55@gmail.com>
2016-12-06 18:29:25 +0100

    Only multicast on selected Interface.

--- client.go
+++ client.go
@@ -14,7 +14,7 @@ var globalGroup net.IP
 var networkInterfaces []net.Interface
 var ignoreAddress []string
 
-func initServer(port int) {
+func initServer(port int, ifc string) {
 	globalPort = port
 	globalGroup = net.IPv4(224, 0, 0, 250)
 
@@ -32,9 +32,13 @@ func initServer(port int) {
 			strs := strings.Split(addr[i].String(), "/")
 			if len(strs) == 2 {
 				fmt.Println("Join on interface", is[i].Name)
-				ignoreAddress = append(ignoreAddress, strs[0]+fmt.Sprint(globalPort))
+				ignoreAddress = append(ignoreAddress, strs[0]+":"+fmt.Sprint(globalPort))
 				networkInterfaces = append(networkInterfaces, is[i])
 				globalConn.JoinGroup(&is[i], &net.UDPAddr{IP: globalGroup})
+				if ifc == is[i].Name {
+					globalConn.SetMulticastInterface(&is[i])
+					fmt.Println("Interface Found")
+				}
 			}
 		}
 	}
@@ -42,11 +46,19 @@ func initServer(port int) {
 	b := make([]byte, 100)
 	for {
 		n, _, src, _ := globalConn.ReadFrom(b)
+
+		own := false
 		for i := 0; i < len(ignoreAddress); i++ {
 			if fmt.Sprint(src) == ignoreAddress[i] {
-				continue
+				own = true
+				break
 			}
 		}
+
+		if own {
+			continue
+		}
+
 		fmt.Println(b, n, src)
 	}
 }
@@ -54,9 +66,9 @@ func initServer(port int) {
 func sendData(data []byte) error {
 	dst := &net.UDPAddr{IP: globalGroup, Port: globalPort}
 	for i := 0; i < len(networkInterfaces); i++ {
-		if err := globalConn.SetMulticastInterface(&networkInterfaces[i]); err != nil {
+		/*if err := globalConn.SetMulticastInterface(&networkInterfaces[i]); err != nil {
 			return err
-		}
+		}*/
 		globalConn.SetMulticastTTL(2)
 		if _, err := globalConn.WriteTo(data, nil, dst); err != nil {
 			return err
